fadfae585c1b9c6cc80c5c4c603306d9f28f16ab
Ndn1618 <ndilbar1618@gmail.com>
2021-02-14 13:07:11 +0500

    Validate documents

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useNewUrlParser: true,
@@ -7,16 +8,36 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 
 const User = mongoose.model('User', {
   name: {
-    type: String
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Email is invalid!')
+      }
+    }
   },
   age: {
-    type: Number
+    type: Number,
+    trim: true,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error('Age must be a positive number!')
+      }
+    }
   }
 })
 
 const me = new User({
-  name: "Dilbar",
-  age: 19
+  name: "        Dilbar",
+  email: "Ndn1618@gmail.com        "
 })
 
 me.save().then(() => {
