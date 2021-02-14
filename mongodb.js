7f76ca672e0bb4406e4d36f83bb35273f87949e9
Ndn1618 <ndilbar1618@gmail.com>
2021-02-14 09:49:25 +0500

    Update documents

--- mongodb.js
+++ mongodb.js
@@ -3,8 +3,6 @@ const { MongoClient, ObjectID } = require('mongodb')
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-const id = new ObjectID()
-
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
   if (error) {
     return console.log('Unable to connect to database!')
@@ -12,11 +10,27 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
   const db = client.db(databaseName)
 
-  db.collection('users').findOne({ name: 'Matluba' }, (error, user) => {
-    console.log(user)
-  })
+  // db.collection('tasks').updateOne({
+  //   _id: new ObjectID("6027e5fdcdf6ff033c33cc9c")
+  // }, {
+  //   $set: {
+  //     description: 'Sleep well'
+  //   }
+  // }).then((result) => {
+  //   console.log('Result', result)
+  // }).catch((error) => {
+  //   console.log('Error', error)
+  // })
 
-  db.collection('tasks').find({ completed: true }).toArray((error, tasks) => {
-    console.log(tasks)
+  db.collection('tasks').updateMany({
+    completed: true
+  }, {
+    $set: {
+      completed: false
+    }
+  }).then((result) => {
+    console.log(result)
+  }).catch((error) => {
+    console.log(error)
   })
 })
