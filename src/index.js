22324329d1404395d6c37c8d243e41744bce7d8d
Ndn1618 <ndilbar1618@gmail.com>
2021-02-18 17:49:24 +0500

    Resource updating users endpoint

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ app.post('/users', async (req, res) => {
 app.get('/users', async (_, res) => {
 
   try {
-    const users = User.find({})
+    const users = await User.find({})
     res.send(users)
   } catch (e) {
     res.status(500).send()
@@ -32,7 +32,7 @@ app.get('/users/:id', async (req, res) => {
   const _id = req.params.id
 
   try {
-    const user = User.findById(_id)
+    const user = await User.findById(_id)
 
     if (!user) {
       return res.status(404).send()
@@ -41,6 +41,28 @@ app.get('/users/:id', async (req, res) => {
     res.send(user)
   } catch (e) {
     res.status(500).send()
+  }
+})
+
+app.patch('/users/:id', async (req, res) => {
+  const updates = Object.keys(req.body)
+  const allowedUpdates = ['name', 'email', 'password', 'age']
+  const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: 'Invalid updates!' })
+  }
+
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true, })
+
+    if (!user) {
+      return res.status(404).send()
+    }
+
+    res.send(user)
+  } catch (e) {
+    res.status(400).send(e)
   }
 })
 
