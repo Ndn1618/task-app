8949afc2e0ea8491dcea7ff1dc4780acfe822d5a
Ndn1618 <ndilbar1618@gmail.com>
2021-02-18 18:17:17 +0500

    Resource deleting endpoints

--- src/index.js
+++ src/index.js
@@ -66,6 +66,20 @@ app.patch('/users/:id', async (req, res) => {
   }
 })
 
+app.delete('/users/:id', async (req, res) => {
+  try {
+    const user = User.findByIdAndDelete(req.params.id)
+
+    if (!user) {
+      return res.status(404).send()
+    }
+
+    res.send(user)
+  } catch (e) {
+    res.status(500).send()
+  }
+})
+
 app.post('/tasks', async (req, res) => {
   const task = new Task(req.body)
 
@@ -118,6 +132,20 @@ app.patch('/tasks/:id', async (req, res) => {
     res.send(task)
   } catch (e) {
     res.status(400).send()
+  }
+})
+
+app.delete('/tasks/:id', async (req, res) => {
+  try {
+    const task = Task.findByIdAndDelete(req.params.id)
+
+    if (!task) {
+      return res.status(404).send()
+    }
+
+    res.send(task)
+  } catch (e) {
+    res.status(500).send()
   }
 })
 
