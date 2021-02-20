c8c86d701e6c817686b38dbaf61d347b4231b1a0
Ndn1618 <ndilbar1618@gmail.com>
2021-02-20 15:49:33 +0500

    Generate Authentication Tokens

--- src/routes/user.js
+++ src/routes/user.js
@@ -8,7 +8,10 @@ router.post('/users', async (req, res) => {
 
   try {
     await user.save()
-    res.status(201).send(user)
+
+    const token = await user.generateAuthToken()
+
+    res.status(201).send({ user, token })
   } catch (e) {
     res.status(400).send(e)
   }
@@ -17,7 +20,10 @@ router.post('/users', async (req, res) => {
 router.post('/users/login', async (req, res) => {
   try {
     const user = await User.findByCredentials(req.body.email, req.body.password)
-    res.send(user)
+
+    const token = await user.generateAuthToken(user)
+
+    res.send({ user, token })
   } catch (e) {
     res.status(400).send()
   }
