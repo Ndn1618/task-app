5e8f51d01bc608e41e910711a864c32ea3bbd8d5
Ndn1618 <ndilbar1618@gmail.com>
2021-02-21 16:50:04 +0500

    Adding images to User Profile

--- src/routes/user.js
+++ src/routes/user.js
@@ -91,7 +91,6 @@ router.delete('/users/me', auth, async (req, res) => {
 })
 
 const upload = multer({
-  dest: 'avatars',
   limits: {
     fileSize: 1000000
   },
@@ -103,7 +102,11 @@ const upload = multer({
   }
 })
 
-router.post('/users/me/avatar', upload.single('avatar'), async (req, res) => {
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+
+  req.user.avatar = req.file.buffer
+  await req.user.save()
+
   res.send()
 }, (error, req, res, next) => {
   res.status(400).send({ error: error.message })
