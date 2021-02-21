f4f0a71e2246f57f1aadf5ce84a5e51f65da90c5
Ndn1618 <ndilbar1618@gmail.com>
2021-02-21 11:46:55 +0500

    Authenticating tasks endpoints

--- src/index.js
+++ src/index.js
@@ -14,18 +14,18 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
 
-const Task = require('./models/task')
-const User = require('./models/user')
+// const Task = require('./models/task')
+// const User = require('./models/user')
 
-const main = async () => {
-  // const task = await Task.findById('6031f1e6835fd13ce09ef528')
-  // await task.populate('owner').execPopulate()
-  // console.log(task.owner)
+// const main = async () => {
+//   // const task = await Task.findById('6031f1e6835fd13ce09ef528')
+//   // await task.populate('owner').execPopulate()
+//   // console.log(task.owner)
 
-  const user = await User.findById('6031f1a7835fd13ce09ef526')
-  await user.populate('tasks').execPopulate()
+//   const user = await User.findById('6031f1a7835fd13ce09ef526')
+//   await user.populate('tasks').execPopulate()
 
-  console.log(user.tasks)
-}
+//   console.log(user.tasks)
+// }
 
-main()
+// main()
