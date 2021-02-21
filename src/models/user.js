dea6299b530a456e9606d25b9b83326a10457b37
Ndn1618 <ndilbar1618@gmail.com>
2021-02-21 09:58:28 +0500

    Hide private data

--- src/models/user.js
+++ src/models/user.js
@@ -52,6 +52,17 @@ const userSchema = new mongoose.Schema({
   ]
 })
 
+userSchema.methods.toJSON = function () {
+  const user = this
+
+  const userObject = user.toObject()
+
+  delete userObject.tokens
+  delete userObject.password
+
+  return userObject
+}
+
 userSchema.methods.generateAuthToken = async function () {
   const user = this
 
