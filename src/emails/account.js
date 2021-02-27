2eb2a5ac801095b50bd32887ef0cd1a9469883a4
Ndn1618 <ndilbar1618@gmail.com>
2021-02-27 21:26:15 +0500

    Sending welcome and cancelation emails

---
+++ src/emails/account.js
@@ -0,0 +1,28 @@
+const sgMail = require('@sendgrid/mail')
+
+SENDGRID_API_KEY = ''
+
+sgMail.setApiKey(SENDGRID_API_KEY)
+
+const sendWelcomeEmail = (email, name) => {
+  sgMail.send({
+    to: email,
+    from: 'dilbarnorqobulova@gmail.com',
+    subject: 'Thanks for joining in',
+    text: `Welcome to the app, ${name}. Let me know how you get along with the app`
+  })
+}
+
+const sendCancelationEmail = (email, name) => {
+  sgMail.send({
+    to: email,
+    from: 'dilbarnorqobulova@gmail.com',
+    subject: 'Sorry to see you go!',
+    text: `Goodbye, ${name}. I hope to see you back sometime soon.`
+  })
+}
+
+module.exports = {
+  sendWelcomeEmail,
+  sendCancelationEmail
+}
--- src/routes/user.js
+++ src/routes/user.js
@@ -4,12 +4,14 @@ const User = require('../models/user')
 const auth = require('../middleware/auth')
 const router = new express.Router()
 const sharp = require('sharp')
+const { sendWelcomeEmail, sendCancelationEmail } = require('../emails/account')
 
 router.post('/users', async (req, res) => {
   const user = new User(req.body)
 
   try {
     await user.save()
+    sendWelcomeEmail(user.email, user.name)
 
     const token = await user.generateAuthToken()
 
@@ -84,6 +86,7 @@ router.patch('/users/me', auth, async (req, res) => {
 router.delete('/users/me', auth, async (req, res) => {
   try {
     await req.user.remove()
+    sendCancelationEmail(req.user.email, req.user.name)
 
     res.send(req.user)
   } catch (e) {
