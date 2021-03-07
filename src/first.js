f4a4a664435cc3252292b369edf21f450ad1f3a4
Ndn1618 <ndilbar1618@gmail.com>
2021-03-07 13:41:49 +0500

    Writing own tests

--- src/first.js
+++ src/first.js
@@ -1,8 +1,11 @@
-const calculateTip = (total, tipPercent) => {
-  const tip = total * tipPercent
-  return total + tip
-}
+const calculateTip = (total, tipPercent) => tip = total * tipPercent + total
+
+const fahrenheitToCelsius = (temp) => (temp - 32) / 1.8
+
+const celsiusToFahrenheit = (temp) => (temp * 1.8) + 32
 
 module.exports = {
-  calculateTip
+  calculateTip,
+  fahrenheitToCelsius,
+  celsiusToFahrenheit,
 }
--- tests/first.test.js
+++ tests/first.test.js
@@ -1,8 +1,18 @@
-const { calculateTip } = require('../src/first')
+const { calculateTip, fahrenheitToCelsius, celsiusToFahrenheit } = require('../src/first')
 
 test('Should calculate total with tip', () => {
 
   const total = calculateTip(10, .3)
   expect(total).toBe(13)
 
+})
+
+test('Should convert 32 F to 0 C', () => {
+  const temp = fahrenheitToCelsius(32)
+  expect(temp).toBe(0)
+})
+
+test('Should convert 0 C to 32 F', () => {
+  const temp = celsiusToFahrenheit(0)
+  expect(temp).toBe(32)
 })
