cb821cfc26fd13e72398b7176880c09fcbafc947
Ndn1618 <ndilbar1618@gmail.com>
2021-03-19 17:15:36 +0500

    Wrapping up User Tests

--- tests/__mocks__/@sendgrid/mail.js
+++ tests/__mocks__/@sendgrid/mail.js
@@ -1,5 +1,5 @@
 module.exports = {
-  serApiKey() {
+  setApiKey() {
 
   },
   send() {
--- tests/user.test.js
+++ tests/user.test.js
@@ -91,3 +91,55 @@ test('Should not delete account for unauthenticated user', async () => {
     .send()
     .expect(401)
 })
+
+test('Should delete account for user', async () => {
+  await request(app)
+    .delete('/users/me')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200)
+
+  const user = await User.findById(userOneId)
+  expect(user).toBeNull()
+})
+
+test('Should not delete account for unauthenticated user', async () => {
+  await request(app)
+    .delete('/users/me')
+    .send()
+    .expect(401)
+})
+
+test('Should upload avatar image', async () => {
+  await request(app)
+    .post('/users/me/avatar')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .attach('avatar', 'tests/fixtures/crocodile.jpg')
+    .expect(200)
+
+  const user = await User.findById(userOneId)
+  expect(user.avatar).toEqual(expect.any(Buffer))
+})
+
+test('Should update valid user fields', async () => {
+  await request(app)
+    .patch('/users/me')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send({
+      name: 'Jess'
+    })
+    .expect(200)
+
+  const user = await User.findById(userOneId)
+  expect(user.name).toEqual('Jess')
+})
+
+test('Should not update invalid fields', async () => {
+  await request(app)
+    .patch('/users/me')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send({
+      location: 'Uzbekistan'
+    })
+    .expect(400)
+})
